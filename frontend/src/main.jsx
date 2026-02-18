71c6528efb66d7bd4fd11b5f10d2ac4033ad3cbc
abhijith <abhijitht210@gmail.com>
2026-02-18 13:52:12 +0530

    typo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Email-OTP-Authentication-System-with-Nodemailer/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
