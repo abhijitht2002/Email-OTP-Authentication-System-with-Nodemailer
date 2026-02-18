bcdf026c58b9d61780d373c00d88ce5054e86751
abhijith <abhijitht210@gmail.com>
2026-02-18 14:01:14 +0530

    typo

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(cors(
   {
-    origin: "https://email-otp-authentication-system-wit.vercel.app/",
+    origin: "https://email-otp-authentication-system-wit.vercel.app",
   }
 ));
 
